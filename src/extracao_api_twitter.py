0cfe1da252e60e0cad551d324e5bba9f44c39e88
Ubuntu <guilherme@ubuntu.aphqrp4iiese5frtgm54bszyba.cx.internal.cloudapp.net>
2023-08-18 01:36:40 +0000

    .

---
+++ src/extracao_api_twitter.py
@@ -0,0 +1,64 @@
+"""from datetime import datetime, timedelta
+import json
+import requests
+import os
+
+TIMESTAMP_FORMAT = "%Y-%m-%dT%H:%M:%S.00Z"
+
+end_time = datetime.now().strftime(TIMESTAMP_FORMAT)
+start_time = (datetime.now() + timedelta(-1)).date().strftime(TIMESTAMP_FORMAT)
+query = "Generative AI"
+
+tweet_fields = "tweet.fields=author_id,conversation_id,created_at,id,in_reply_to_user_id,public_metrics,lang,text"
+user_fields = "expansions=author_id&user.fields=id,name,username,created_at"
+
+url_raw = f"https://api.twitter.com/2/tweets/search/recent?query={query}&{tweet_fields}&{user_fields}&start_time={start_time}&end_time={end_time}"
+
+bearer_token = os.environ.get("BEARER_TOKEN")
+headers = {"Authorization":"Bearer {}".format(bearer_token)}
+response = requests.request("GET", url_raw, headers=headers)
+
+json_response = response.json()
+
+print(json.dumps(json_response, indent=4, sort_keys=True))
+
+"""
+import requests
+import os
+import json
+
+# To set your environment variables in your terminal run the following line:
+# export 'BEARER_TOKEN'='<your_bearer_token>'
+bearer_token = os.environ.get("BEARER_TOKEN")
+
+search_url = "https://api.twitter.com/2/tweets/search/recent"
+
+# Optional params: start_time,end_time,since_id,until_id,max_results,next_token,
+# expansions,tweet.fields,media.fields,poll.fields,place.fields,user.fields
+query_params = {'query': '(from:twitterdev -is:retweet) OR #twitterdev','tweet.fields': 'author_id'}
+
+
+def bearer_oauth(r):
+    """
+    Method required by bearer token authentication.
+    """
+
+    r.headers["Authorization"] = f"Bearer {bearer_token}"
+    r.headers["User-Agent"] = "v2RecentSearchPython"
+    return r
+
+def connect_to_endpoint(url, params):
+    response = requests.get(url, auth=bearer_oauth, params=params)
+    print(response.status_code)
+    if response.status_code != 200:
+        raise Exception(response.status_code, response.text)
+    return response.json()
+
+
+def main():
+    json_response = connect_to_endpoint(search_url, query_params)
+    print(json.dumps(json_response, indent=4, sort_keys=True))
+
+
+if __name__ == "__main__":
+    main()
