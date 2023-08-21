4421bc7ce7ba4200876b6d2c93acccd7f6056ce3
Ubuntu <guilherme@ubuntu.aphqrp4iiese5frtgm54bszyba.cx.internal.cloudapp.net>
2023-08-21 17:48:11 +0000

    .

--- airflow_pipeline/dags/twitter_dag.py
+++ airflow_pipeline/dags/twitter_dag.py
@@ -3,6 +3,7 @@ sys.path.append("airflow_pipeline")
 
 from airflow.models import DAG
 from operators.twitter_operator import TwitterOperator
+#from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 from os.path import join
 from airflow.utils.dates import days_ago
     
@@ -17,3 +18,12 @@ with DAG(dag_id = "TwitterDAG", start_date=days_ago(60), schedule_interval="@dai
                             start_time="{{ data_interval_start.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}", 
                             end_time="{{ data_interval_end.strftime('%Y-%m-%dT%H:%M:%S.00Z') }}",
                             task_id=f"twitter_{query}")
+ 
+    """twitter_transform = SparkSubmitOperator(task_id="transform_twitter_datascience",
+                                            application="/home/guilherme/extracao-dados-twitter/src/spark/transformation.py",
+                                            name="twitter_transformation",
+                                            application_args=["--src","/home/guilherme/extracao-dados-twitter/datalake/twitter_datascience",
+                                                              "--dest","/home/guilherme/extracao-dados-twitter/dados_transformation",
+                                                              "--process-date","{{ ds }}"])"""
+    
+#twitter_operator >> twitter_transform
